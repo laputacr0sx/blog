53b899d60ceea894af42335f42064aafef7d4290
veilicsVV <laputafelix@gmail.com>
2022-04-04 15:29:08 +0800

    D04042022T1528 Add blog button added

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import React, { useContext } from 'react';
+
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+	const { data, addBlogPost } = useContext(BlogContext);
+
+	return (
+		<View style={{ flex: 1 }}>
+			<Text>IndexScreen Showed</Text>
+			<Button
+				title='Add Post'
+				onPress={() => {
+					addBlogPost();
+				}}></Button>
+			<FlatList
+				data={data}
+				keyExtractor={(blogPost) => {
+					return blogPost.title;
+				}}
+				renderItem={({ item }) => {
+					return <Text>{item.title}</Text>;
+				}}
+			/>
+		</View>
+	);
+};
+
+export default IndexScreen;
+
+const styles = StyleSheet.create({});
